042d0f57e56e1d929f9ab60566013f96ff8c6824
Turchanov-Denis <turchanovdenis786@gmail.com>
2026-04-11 13:37:54 +0200

    init Get Post -> next write mux

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "sync"
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+
+	_ "modernc.org/sqlite"
+)
 
 type User struct {
 	ID     string `json:"id"`
@@ -59,7 +67,7 @@ func (c *LRUCache) Put(key string, value User) {
 	}
 }
 
-func (c *LRUCache) moveToFront(node *node) {
+func (c *LRUCache) moveToFront(n *node) {
 	c.remove(n)
 	c.addToFront(n)
 }
@@ -98,6 +106,73 @@ func (c *LRUCache) removeOldest() {
 	c.remove(oldest)
 	delete(c.items, oldest.key)
 }
+
+var db *sql.DB
+var cache = NewLRUCache(1000)
+
+func initDB() {
+	var err error
+	db, err = sql.Open("sqlite", "users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, name TEXT, avatar BLOB)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var u User
+
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	if u.ID == "" || len(u.Avatar) == 0 {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+	if len(u.Avatar) > 20*1024 {
+		http.Error(w, "avatar so large", http.StatusBadRequest)
+	}
+	_, err := db.Exec("INSERT OR REPLACE INTO users(id, name, avatar) VALUES(?, ?, ?)")
+
+	if err != nil {
+		http.Error(w, "db error", http.StatusInternalServerError)
+		return
+	}
+	cache.Put(u.ID, u)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "id required", http.StatusBadRequest)
+		return
+	}
+	if u, ok := cache.Get(id); ok {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(u)
+		return
+	}
+
+	var u User
+	err := db.QueryRow("SELECT id, name, avatar FROM users WHERE id = ?", id).Scan(&u.ID, &u.Name, &u.Avatar)
+	if err == sql.ErrNoRows {
+		http.NotFound(w, r)
+	}
+	if err != nil {
+		http.Error(w, "db error", http.StatusInternalServerError)
+	}
+	cache.Put(id, u)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(u)
+}
+
 func main() {
+	initDB()
 
 }
